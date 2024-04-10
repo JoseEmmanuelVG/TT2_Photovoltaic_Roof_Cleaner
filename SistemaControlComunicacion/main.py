911c5bb352e8e6d0302b47f6067fc884fee38cbc
JoseEmmanuelVG <89156254+JoseEmmanuelVG@users.noreply.github.com>
2024-04-10 23:14:47 +0000

    solving the errors

--- SistemaControlComunicacion/main.py
+++ SistemaControlComunicacion/main.py
@@ -9,3 +9,6 @@ callbacks.register_callbacks(app)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+# sudo /home/ttm/TT2_Photovoltaic_Roof_Cleaner/entorno_jevg/bin/python /home/ttm/TT2_Photovoltaic_Roof_Cleaner/SistemaControlComunicacion/main.py
