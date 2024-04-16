a91be5b4178e08aaa02799fad0e3d6d60e74da4a
JoseEmmanuelVG <jevgjosemmanuel@gmail.com>
2024-04-16 13:55:53 -0600

    Update UI corroborated movements

--- SistemaControlComunicacion/ui_definition.py
+++ SistemaControlComunicacion/ui_definition.py
@@ -3,11 +3,27 @@ from dash import Dash, html, dcc
 def create_layout():
     return html.Div([
         html.H1("Control del Robot"),
-        html.Button('Adelante', id='forward-btn'),
-        html.Button('Atrás', id='backward-btn'),
-        html.Button('Izquierda', id='left-btn'),
-        html.Button('Derecha', id='right-btn'),
-        html.Button('Rotar Derecha', id='rotate-right-btn'),
-        html.Button('Rotar Izquierda', id='rotate-left-btn'),
-        html.Div(id='status-div')
+        html.Div([
+            html.Div(id='status-div', style={'grid-area': 'status'}),  # Div para mensajes de estado
+            html.Button('Adelante', id='forward-btn', style={'grid-area': 'forward'}),
+            html.Button('Atrás', id='backward-btn', style={'grid-area': 'backward'}),
+            html.Button('Izquierda', id='left-btn', style={'grid-area': 'left'}),
+            html.Button('Derecha', id='right-btn', style={'grid-area': 'right'}),
+            html.Button('Rotar Derecha', id='rotate-right-btn', style={'grid-area': 'rotate-right'}),
+            html.Button('Rotar Izquierda', id='rotate-left-btn', style={'grid-area': 'rotate-left'}),
+        ], style={
+            'display': 'grid',
+            'grid-template-areas': '''
+                " . forward . "
+                "left . right"
+                "rotate-left . rotate-right"
+                " . backward . "
+                " . status . "
+            ''',
+            'grid-gap': '10px',
+            'justify-content': 'center',
+            'align-items': 'center',
+            'padding': '20px'
+        })
     ])
+
